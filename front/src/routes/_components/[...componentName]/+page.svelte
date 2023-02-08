4283b7be9ddee947b31d5da1f7d9f20ead80a74c
Ewen Le Bihan <lebihae@bde.enseeiht.fr>
2023-02-08 02:01:07 +0100

    🎉 Setup SvelteKit

---
+++ front/src/routes/_components/[...componentName]/+page.svelte
@@ -0,0 +1,110 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	let [componentName, slotContent] = $page.params['componentName'].split('/', 2);
+	let props = Object.fromEntries(
+		Array(...$page.url.searchParams.entries()).map(([k, v]) => {
+			try {
+				return [k, JSON.parse(v)];
+			} catch (e) {
+				console.log(v);
+				return [k, true];
+			}
+		})
+	);
+
+	let wireframe = false;
+	let componentDomNode: HTMLElement;
+	let newPropKey = '';
+	let newPropValue = '';
+</script>
+
+<h1>
+	&lt;{componentName}{#if props.length > 0}:{/if}{Object.entries(props).map(
+		([k, v]) => `${k}=${JSON.stringify(v)}`
+	)}{#if slotContent}
+		&gt;{slotContent}&lt;/{componentName}&gt;
+	{:else}
+		/&gt;
+	{/if}
+</h1>
+
+{#await import(`../../../lib/${componentName}.svelte`) then component}
+	<main class:wireframe bind:this={componentDomNode}>
+		{#if slotContent}
+			<svelte:component this={component.default} {...props}>
+				{slotContent}
+			</svelte:component>
+		{:else}
+			<svelte:component this={component.default} {...props} />
+		{/if}
+	</main>
+	<section class="props">
+		{#each Object.entries(props) as [key, value] (key)}
+			<label for={`prop-${key}`}
+				>{key}
+				<input id={`prop-${key}`} type="text" bind:value={props[key]} />
+				<button on:click={() => delete props[key]}>Delete</button>
+			</label>
+		{/each}
+
+		<div class="new-prop">
+			<p>New prop</p>
+			<input type="text" bind:value={newPropKey} />
+			<input type="text" bind:value={newPropValue} />
+			<button
+				on:click={() => {
+					props[newPropKey] = JSON.parse(newPropValue);
+					newPropKey = '';
+					newPropValue = '';
+				}}>Add</button
+			>
+		</div>
+
+		<label class="wireframe-toggle"
+			>Show wireframe
+			<input type="checkbox" bind:checked={wireframe} />
+		</label>
+	</section>
+	{#if wireframe}
+		<section class="info">
+			<code
+				>{componentDomNode?.getBoundingClientRect().width - 2} × {componentDomNode?.getBoundingClientRect()
+					.height - 2}</code
+			>
+		</section>
+	{/if}
+{:catch error}
+	<section class="errored">
+		<h2>Woops!</h2>
+		<p>An error occured</p>
+		<pre><code>{error}</code></pre>
+	</section>
+{/await}
+
+<style>
+	section.errored {
+		background: red;
+		color: white;
+		padding: 1em 2em;
+		border-radius: 0.5em;
+	}
+
+	main.wireframe {
+		border-color: black;
+	}
+	main {
+		border: 1px solid transparent;
+		display: inline-flex;
+		box-sizing: content-box;
+	}
+
+	section.props {
+		position: fixed;
+		bottom: 1em;
+		left: 1em;
+		display: flex;
+		flex-direction: column;
+		gap: 0.5em;
+	}
+</style>
---
+++ front/src/routes/_showcase/icons/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import Icon from '$lib/Icon.svelte';
+	import iconNames from './all_icon_stems';
+</script>
+
+<h1>Icons</h1>
+
+<main>
+	{#each iconNames as name}
+		<div class="item">
+			<div class="icon">
+				<Icon {name} color="fg" />
+			</div>
+			<p class="typo-field-label">{name.replaceAll('-', ' ')}</p>
+		</div>
+	{/each}
+</main>
+
+<style>
+	:global(body) {
+		margin: 0 auto;
+		max-width: 1000px;
+	}
+
+	main {
+		display: flex;
+		flex-wrap: wrap;
+        align-self: center;
+		width: 100%;
+		row-gap: 3rem;
+        column-gap: 5rem;
+	}
+
+	.item {
+		display: flex;
+		flex-direction: column;
+        align-items: center;
+		width: 3rem;
+	}
+
+	.icon {
+		width: 3rem;
+	}
+</style>
