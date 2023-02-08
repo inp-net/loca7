126448eb5e1982b4f84e3a24b95d3abc2c1bac73
Ewen Le Bihan <hey@ewen.works>
2023-02-08 14:57:54 +0100

    🔨 Add showcases for colors and typography

---
+++ front/src/routes/_showcase/colors/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	const names = [
+		['fg', 'muted', 'bg'],
+		['ice', 'diamond', 'sky', 'cobalt'],
+		['moss', 'acid', 'cactus'],
+		['plaster', 'gold', 'safran'],
+		['rose', 'mushroom', 'blood']
+	];
+</script>
+
+<h1>Colors</h1>
+
+<main>
+	{#each names as row}
+		<div class="row">
+			{#each row as name}
+				<div class="item">
+					<div class="swatch" style="background-color: var(--{name})" />
+					<p class="typo-field-label">{name}</p>
+				</div>
+			{/each}
+		</div>
+	{/each}
+</main>
+
+<style>
+	:global(body) {
+		margin: 0 auto;
+		max-width: 1000px;
+	}
+
+	h1 {
+		margin-bottom: 5rem;
+	}
+
+	main {
+		display: flex;
+		flex-wrap: wrap;
+		align-self: center;
+		flex-direction: column;
+		width: 100%;
+		row-gap: 2rem;
+	}
+
+	.row {
+		display: flex;
+		flex-direction: row;
+		gap: 5rem;
+	}
+
+	.item {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 6rem;
+	}
+
+	.swatch {
+		width: 6rem;
+		height: 6rem;
+		margin-bottom: 1rem;
+	}
+</style>
---
+++ front/src/routes/_showcase/typography/+page.svelte
@@ -0,0 +1,26 @@
+<main>
+	<h1 class="typo-title">Title</h1>
+
+	<p class="typo-paragraph">A paragraph with <a href="https://media.ewen.works/rickroll.mp4">an inline link</a></p>
+
+	<label class="typo-field-label">Field label</label>
+
+	<span class="typo-big-figure">30 000€</span>
+
+	<p class="typo-details">Details</p>
+
+	<p class="typo-big-button">Big button label</p>
+
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+	}
+
+	main > * {
+		display: block;
+	}
+</style>
