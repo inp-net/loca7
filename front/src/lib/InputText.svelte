342ec314f3c813d50c3111bebd008d4a6e5f6f5c
Ewen Le Bihan <hey@ewen.works>
2023-02-11 21:01:04 +0100

    ✨ Implement \<InputText\>

---
+++ front/src/lib/InputText.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import BaseInputText from './BaseInputText.svelte';
+
+	export let id: string | undefined = undefined;
+	export let name: string | undefined = undefined;
+	export let initial: string | undefined = undefined;
+	export let placeholder: string | undefined = undefined;
+	export let value: string;
+</script>
+
+<BaseInputText type="text" bind:value {id} {name} {initial} {placeholder} />
