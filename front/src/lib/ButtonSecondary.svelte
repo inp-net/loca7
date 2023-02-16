e4ad48cbac36af38331b0891ea5d612037aaab70
Ewen Le Bihan <hey@ewen.works>
2023-02-16 20:26:24 +0100

    ✨ <ButtonSecondary> Support using hrefs, reduce padding

--- front/src/lib/ButtonSecondary.svelte
+++ front/src/lib/ButtonSecondary.svelte
@@ -2,15 +2,17 @@
 	import { createEventDispatcher } from 'svelte';
 	import Icon from './Icon.svelte';
 
-	export let icon: string = "";
-	export let id: string = "";
+	export let icon: string = '';
+	export let id: string = '';
+	export let href: string = '';
 	export let dangerous: boolean = false;
 
 	let hoverOrFocus = false;
 </script>
 
-<button
-	class="typo-paragraph"
+<svelte:element
+	this={href ? 'a' : 'button'}
+	class="button-secondary typo-paragraph"
 	class:dangerous
 	{id}
 	on:focus={() => (hoverOrFocus = true)}
@@ -25,14 +27,14 @@
 		</div>
 	{/if}
 	<slot />
-</button>
+</svelte:element>
 
 <style>
-	button {
+	.button-secondary {
 		display: flex;
 		align-items: center;
 		gap: 0.5em;
-		padding: 0.5rem 1rem;
+		padding: 0.25rem 0.75rem;
 
 		background: var(--bg);
 		border: var(--border-width) solid var(--fg);
@@ -41,25 +43,26 @@
 		cursor: pointer;
 	}
 
-	button:hover,
-	button:focus {
+	.button-secondary:hover,
+	.button-secondary:focus {
 		background: var(--fg);
 		color: var(--bg);
 	}
 
-	button.dangerous {
+	.button-secondary.dangerous {
 		border-color: var(--mushroom);
 		background-color: var(--mushroom);
 		color: #fff;
 	}
 
-	button.dangerous:hover,
-	button.dangerous:focus {
+	.button-secondary.dangerous:hover,
+	.button-secondary.dangerous:focus {
 		background-color: var(--blood);
 		border-color: var(--blood);
 	}
 
 	.icon {
 		height: 1em;
+		width: 1em;
 	}
 </style>
