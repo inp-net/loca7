eab2393e20c9046104459f0c1839583975934e7d
Ewen Le Bihan <hey@ewen.works>
2023-02-17 02:41:12 +0100

    🚧 Start implementing <InputAdress>

---
+++ front/src/lib/InputAddress.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import BaseInputText from './BaseInputText.svelte';
+
+	export let unit: string = '';
+	export let value: string;
+	export let id: string | undefined = undefined;
+	export let name: string | undefined = undefined;
+	export let initial: string | undefined = undefined;
+	export let placeholder: string = '';
+
+	let errorMessage: string = '';
+
+	let suggestions: string[] = [];
+	$: suggestions = ['aaa', 'bbb', 'ccc'];
+
+	// TODO implement adress validation/autocompletion
+</script>
+
+<BaseInputText
+	{suggestions}
+	{placeholder}
+	type="number"
+	{unit}
+	bind:value
+	{id}
+	{name}
+	{initial}
+	{errorMessage}
+/>
