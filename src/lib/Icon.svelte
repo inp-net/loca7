b0e39c7a0739abd43b7d9c403ba8fceb6da5ed76
Ewen Le Bihan <lebihae@bde.enseeiht.fr>
2023-03-05 16:50:13 +0100

    🐛 Fix layout shift when icons have not loaded yet
    On the long term, the compiler should replace <Icon name="..."> with <icons/...> to prevent this at all

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -6,7 +6,9 @@
 	export let cancel = false;
 </script>
 
-{#await import(`./icons/${name}${cancel ? '-cancel' : ''}.svelte`) then iconSVG}
+{#await import(`./icons/${name}${cancel ? '-cancel' : ''}.svelte`)}
+	<svg class="loading-icon" height="50" width="50" viewBox="0 0 50 50" />
+{:then iconSVG}
 	<svelte:component
 		this={iconSVG.default}
 		color={color.startsWith('#') ? color : `var(--${color})`}
