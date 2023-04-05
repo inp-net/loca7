33149e36c2280d2899b185ad26421cf568645b74
Ewen Le Bihan <lebihae@bde.enseeiht.fr>
2023-04-05 23:58:28 +0200

    ✨ JSON API: add link to individual appartment pages

--- src/routes/appartements.json/+server.ts
+++ src/routes/appartements.json/+server.ts
@@ -1,6 +1,7 @@
 import type { RequestHandler } from './$types';
 import { prisma } from '$lib/server/prisma';
 import { jsonAPIOutputsInclude } from '$lib/types';
+import { env } from 'process';
 
 export const GET: RequestHandler = async ({ params, locals }) => {
 	const appartments = await prisma.appartment.findMany({
@@ -11,9 +12,14 @@ export const GET: RequestHandler = async ({ params, locals }) => {
 		include: jsonAPIOutputsInclude
 	});
 
-	return new Response(JSON.stringify(appartments), {
-		headers: {
-			'Content-Type': 'application/json'
+	return new Response(
+		JSON.stringify(
+			appartments.map((a) => ({ link: `${env.ORIGIN || "http://localhost:5173"}/appartements/${a.number}.json`, ...a }))
+		),
+		{
+			headers: {
+				'Content-Type': 'application/json'
+			}
 		}
-	});
+	);
 };
