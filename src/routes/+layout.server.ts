06cafa37330f461af6f2c56a13c0a7bad937cda2
Ewen Le Bihan <lebihae@bde.enseeiht.fr>
2023-04-09 00:40:46 +0200

    🚑️ Fix missing prisma import

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,5 @@
 import type { LayoutServerLoad } from './$types';
+import { prisma } from "$lib/server/prisma";
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	const { user, session } = await locals.validateUser();
