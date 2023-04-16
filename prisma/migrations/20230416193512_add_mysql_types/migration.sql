a9b26293f1f34f1c9c80de5a6152448a7bb6ec7c
whidix <theodor.vallier@gmail.com>
2023-04-16 21:37:10 +0200

    add mysql types

---
+++ prisma/migrations/20230416193512_add_mysql_types/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE `Appartment` MODIFY `description` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `AppartmentEdit` MODIFY `description` TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Photo` MODIFY `filename` TINYTEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Report` MODIFY `message` TEXT NOT NULL;
