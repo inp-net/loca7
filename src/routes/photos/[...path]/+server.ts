9471d126c8da788d0a04293ac20d902f51e39520
Ewen Le Bihan <lebihae@bde.enseeiht.fr>
2023-04-23 01:10:10 +0200

    🐛 Fix lookup path for /photos handler

--- src/routes/photos/[...path]/+server.ts
+++ src/routes/photos/[...path]/+server.ts
@@ -1,8 +1,9 @@
 import { publicPath } from '$lib/server/utils';
 import fs from 'fs';
 import type { RequestHandler } from '../$types';
+import path from 'path';
 
 export const GET: RequestHandler = ({ params }) => {
-	const data = fs.readFileSync(publicPath(params.path));
+	const data = fs.readFileSync(publicPath(path.join('photos', params.path)));
 	return new Response(data);
 };
