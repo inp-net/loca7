ee2ebd21e9baa645a14458b855c11097ee1c916d
Ewen Le Bihan <lebihae@bde.enseeiht.fr>
2023-05-03 13:36:00 +0200

    ✨ annoucement: Add /announcement/unreachable
    To maybe contact by SMS/Phone owners that don't have an email

---
+++ src/routes/announcement/unreachable/+server.ts
@@ -0,0 +1,27 @@
+import { guards } from '$lib/server/lucia';
+import { isGhostEmail } from '$lib/types';
+import type { RequestHandler } from './$types';
+import { prisma } from '$lib/server/prisma';
+
+export const GET: RequestHandler = async ({ url, locals, params }) => {
+	const { user, session } = await locals.validateUser();
+	guards.isAdmin(user, session, url);
+
+	const users = (
+		await prisma.user.findMany({
+			where: {
+				keys: {
+					none: {
+						id: {
+							startsWith: 'email:'
+						}
+					}
+				}
+			}
+		})
+	).filter((user) => isGhostEmail(user.email));
+
+	return new Response(
+		users.map((u) => u.phone + '\t' + (u.firstName + ' ' + u.lastName).trim()).join('\n')
+	);
+};
